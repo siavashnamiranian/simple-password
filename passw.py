d286cc2cb76077af06d3053518f51c3c6d3f50c1
siavashnamiranian <68529832+siavashnamiranian@users.noreply.github.com>
2020-07-20 11:02:16 +0430

    Create passw.py
    main app

---
+++ passw.py
@@ -0,0 +1,25 @@
+import random
+weak = ['a','b', 'c', 'd', 'e', 'f','g','h','i','j',
+        'k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+medium = ['a','b', 'c', 'd', 'e', 'f','g','h','i','j',
+          'k','l','m','n','o','p','q','r','s','t','u','v',
+          'w','x','y','z','1','2','3','4','5','6','7','8','9','0']
+strong = ['a','b', 'c', 'd', 'e', 'f','g','h','i','j',
+          'k','l','m','n','o','p','q','r','s','t','u','v',
+          'w','x','y','z','1','2','3','4','5','6','7','8',
+          '9','0','/','#','-','_','%','&','@','$']
+def passw(pass_lenght,strenght):
+    if strenght=="weak":
+        p =  "".join(random.sample(weak,pass_lenght ))
+        return p
+    elif strenght=="average":
+        pass_lenght_average=pass_lenght
+        p =  "".join(random.sample(medium,pass_lenght_average))
+        return p
+    elif strenght=="hard":
+        pass_lenght_hard=pass_lenght
+        p =  "".join(random.sample(strong,pass_lenght_hard))
+        return p
+    else:
+        print(" wrong_input ")
+print(passw(8,"l"))
